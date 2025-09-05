9758a59eac758c9b765c84d2f3d057470056193d
the-hi <161976364+the-hi@users.noreply.github.com>
2025-09-05 16:46:44 +0400

    batman

--- utils/Webhook.js
+++ utils/Webhook.js
@@ -2,10 +2,23 @@ import { config } from '../Config.js';
 import { WebhookClient } from 'discord.js';
 
 let webhookClient;
-if (config.id !== "" && config.token !== "") {
-    webhookClient = new WebhookClient(config);
-} else {
-    console.log("Webhook isnt set up yet.")
+
+if (config.id !== '' && config.token !== '') {
+    webhookClient = new WebhookClient({ id: config.id, token: config.token });
+
+    // Check if webhook actually exists
+    try {
+        await webhookClient.send({
+            content: "Webhook connection intiated 🦇",
+            username: "Skk",
+            avatarURL: "https://cdn.wallpapersafari.com/64/11/WkyqrX.jpg"
+        });
+    } catch {
+        webhookClient = undefined;
+        console.log("Webhook credentials are incorrect.");
+    }
+  } else {
+    console.log("Webhook isn't set up yet.");
 }
 
 export { webhookClient };
