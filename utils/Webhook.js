4ce03bf8adf7def32b30a12b393f2df3d9654205
the-hi <161976364+the-hi@users.noreply.github.com>
2025-09-05 16:00:31 +0400

    batman

--- utils/Webhook.js
+++ utils/Webhook.js
@@ -2,7 +2,7 @@ import { config } from '../Config.js';
 import { WebhookClient } from 'discord.js';
 
 let webhookClient;
-if (config.id && config.token) {
+if (config.id !== "" && config.token !== "") {
     webhookClient = new WebhookClient(config);
 } else {
     console.log("Webhook isnt set up yet.")
