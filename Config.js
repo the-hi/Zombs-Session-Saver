1118b1dc0f84a76a9755e759ef277d823efe5dff
DasLilGuy <m.c.g.u.y.lil794@gmail.com>
2025-09-15 13:39:35 -0400

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@ const config = {
     killCamResolution: 720, // resolution in px (highest resolution varies accoridng to discord limits)
     id: '', // id of the webhook for killCam
     token: '', // token of the webhook for killCam
-    sessionSaverADMIN: 'DIPER_IS_A_SKID', // change this password and keep it to urself, the client script need to be changed in order to use admin
+    sessionSaverADMIN: 'GAY_PLANE_IS_NOOB', // change this password and keep it to urself, the client script need to be changed in order to use admin
     sessionSaverNORMAL: 'JKING_IS_A_SKID', // this password is for normal users
     sessionSaverVIEW: 'GAY_LATIOS_IS_A_SKID', // VIEW only users
     PORT: 8080 // PORT where the session saver runs
